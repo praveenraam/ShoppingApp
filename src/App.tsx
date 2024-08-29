9483f47926d0fd27aa5a0d9f36fdededbaa8b31e
praveenraam <ckpraveenraam@gmail.com>
2024-08-29 09:06:25 +0530

    Project was initiated

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, SafeAreaView, StyleSheet,Text } from 'react-native';
+
+function App() {
+  return (
+    <Text>Google</Text>
+  );
+}
+
+export default App;
